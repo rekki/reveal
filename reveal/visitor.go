d7689545202e8ef3ffc2eaa59c4cbdf63fab273f
Aymeric Beaumet <hi@aymericbeaumet.com>
2022-02-02 15:22:02 +0100

    wip

--- reveal/visitor.go
+++ reveal/visitor.go
@@ -24,12 +24,13 @@ type Endpoint struct {
 }
 
 type Visitor struct {
-	pkg  *packages.Package
-	Root *Group
+	pkg   *packages.Package
+	edges map[*ast.Object]ast.Expr // maps idents to exprs
+	Root  *Group
 }
 
 func NewVisitor(pkg *packages.Package) *Visitor {
-	return &Visitor{pkg, &Group{"/", nil}}
+	return &Visitor{pkg, map[*ast.Object]ast.Expr{}, &Group{"/", nil}}
 }
 
 func (v *Visitor) Walk() {
@@ -95,7 +96,7 @@ func (v *Visitor) getEngines(fdecl *ast.FuncDecl) []*ast.Ident {
 		if valuespec, ok := n.(*ast.ValueSpec); ok {
 			for _, ident := range valuespec.Names {
 				if def := v.pkg.TypesInfo.Defs[ident]; def != nil {
-					if kind := resolveGinKind(def.Type()); kind != Engine {
+					if kind := resolveGinKind(def.Type()); kind == Engine {
 						out = append(out, ident)
 					}
 				}
@@ -111,6 +112,28 @@ func (v *Visitor) getEngines(fdecl *ast.FuncDecl) []*ast.Ident {
 
 func (v *Visitor) collectEndpoints(group *Group, file *ast.File, engine types.Object) {
 	ast.Inspect(file, func(n ast.Node) bool {
+		if assignstmt, ok := n.(*ast.AssignStmt); ok {
+			for i, lhs := range assignstmt.Lhs {
+				if i >= len(assignstmt.Rhs) {
+					break
+				}
+				if ident, ok := lhs.(*ast.Ident); ok {
+					v.edges[ident.Obj] = assignstmt.Rhs[i]
+				}
+			}
+			return false
+		}
+
+		if valuespec, ok := n.(*ast.ValueSpec); ok {
+			for i, ident := range valuespec.Names {
+				if i >= len(valuespec.Values) {
+					break
+				}
+				v.edges[ident.Obj] = valuespec.Values[i]
+			}
+			return false
+		}
+
 		callexpr, ok := n.(*ast.CallExpr)
 		if !ok {
 			return true // go deeper until we find a call expression
@@ -130,35 +153,44 @@ func (v *Visitor) collectEndpoints(group *Group, file *ast.File, engine types.Ob
 			return false
 		}
 
-		if obj, ok := v.pkg.TypesInfo.Uses[x]; !ok || obj != engine {
-			return false
-		}
+		if use, ok := v.resolveObject(x); ok && use == engine {
+			switch selector.Sel.Name {
+			case "Group":
+				// TODO: gather nested nodes
 
-		switch selector.Sel.Name {
-		case "Group":
-			// TODO: gather nested nodes
-
-		case "Handle":
-			if len(callexpr.Args) > 1 {
-				m := constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[0]].Value)
-				p, pp := inferPath(constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[1]].Value))
-				if len(m) > 0 && len(p) > 0 {
-					group.Endpoints = append(group.Endpoints, &Endpoint{Method: m, Path: p, PathParams: pp})
+			case "Handle":
+				if len(callexpr.Args) > 1 {
+					m := constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[0]].Value)
+					p, pp := inferPath(constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[1]].Value))
+					if len(m) > 0 && len(p) > 0 {
+						group.Endpoints = append(group.Endpoints, &Endpoint{Method: m, Path: p, PathParams: pp})
+					}
 				}
-			}
 
-		case "POST", "GET", "HEAD", "PUT", "PATCH", "DELETE", "OPTIONS":
-			if len(callexpr.Args) > 0 {
-				m := selector.Sel.Name
-				p, pp := inferPath(constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[0]].Value))
-				if len(m) > 0 && len(p) > 0 {
-					group.Endpoints = append(group.Endpoints, &Endpoint{Method: m, Path: p, PathParams: pp})
+			case "POST", "GET", "HEAD", "PUT", "PATCH", "DELETE", "OPTIONS":
+				if len(callexpr.Args) > 0 {
+					m := selector.Sel.Name
+					p, pp := inferPath(constant.StringVal(v.pkg.TypesInfo.Types[callexpr.Args[0]].Value))
+					if len(m) > 0 && len(p) > 0 {
+						group.Endpoints = append(group.Endpoints, &Endpoint{Method: m, Path: p, PathParams: pp})
+					}
 				}
 			}
 		}
 
 		return false
 	})
+}
+
+func (v *Visitor) resolveObject(x *ast.Ident) (types.Object, bool) {
+	if resolved, ok := v.edges[x.Obj]; ok {
+		if ident, ok := resolved.(*ast.Ident); ok {
+			return v.resolveObject(ident)
+		}
+	}
+
+	object, ok := v.pkg.TypesInfo.Uses[x]
+	return object, ok
 }
 
 var inferPathRegexp = regexp.MustCompilePOSIX(`\/[*:][^\/]+`)
@@ -224,209 +256,3 @@ func resolveGinKind(ty types.Type) GinKind {
 
 	return Unsupported
 }
-
-//func extractGroup(n ast.Node, pkg *packages.Package) (*Group, bool) {
-//callexpr, ok := n.(*ast.CallExpr)
-//if !ok {
-//return nil, false
-//}
-
-//selector, ok := callexpr.Fun.(*ast.SelectorExpr)
-//if !ok {
-//return nil, false
-//}
-
-//var x *ast.Ident
-//if ident, ok := selector.X.(*ast.Ident); ok {
-//x = ident
-//} else if selectorexpr, ok := selector.X.(*ast.SelectorExpr); ok {
-//x = selectorexpr.Sel
-//} else {
-//return nil, false
-//}
-
-//kind := resolveGinKind(pkg.TypesInfo.Uses[x].Type())
-//if kind == Unsupported {
-//return nil, false
-//}
-
-//if selector.Sel.Name != "Group" {
-//return nil, false
-//}
-
-//if len(callexpr.Args) < 1 {
-//return nil, false
-//}
-
-//httpPath, httpPathParams, err := extractPathAndPathParams(callexpr.Args[0], pkg)
-//if err != nil {
-//return nil, false
-//}
-
-//return &Group{
-//ASTNode:    n,
-//Path:       httpPath,
-//PathParams: httpPathParams,
-//}, true
-//}
-
-//func extractEndpoint(n ast.Node, pkg *packages.Package, gitRoot, githubUserRepo, gitHash string) (*Endpoint, bool) {
-//callexpr, ok := n.(*ast.CallExpr)
-//if !ok {
-//return nil, false
-//}
-
-//selector, ok := callexpr.Fun.(*ast.SelectorExpr)
-//if !ok {
-//return nil, false
-//}
-
-//var x *ast.Ident
-//if ident, ok := selector.X.(*ast.Ident); ok {
-//x = ident
-//} else if selectorexpr, ok := selector.X.(*ast.SelectorExpr); ok {
-//x = selectorexpr.Sel
-//} else {
-//return nil, false
-//}
-
-//kind := resolveGinKind(pkg.TypesInfo.Uses[x].Type())
-//if kind == Unsupported {
-//return nil, false
-//}
-
-//// get first arg (the http path) / last arg (the handler)
-//if len(callexpr.Args) < 2 {
-//return nil, false
-//}
-//handlerArg := callexpr.Args[len(callexpr.Args)-1]
-//handlerArgStartPos := pkg.Fset.Position(handlerArg.Pos())
-//handlerArgEndPos := pkg.Fset.Position(handlerArg.End())
-
-//// get the http method and define which parameter is to be the route
-//var httpMethod string
-//pathArg := callexpr.Args[0]
-//switch selector.Sel.Name {
-//case "POST":
-//httpMethod = http.MethodPost
-//case "GET":
-//httpMethod = http.MethodGet
-//case "HEAD":
-//httpMethod = http.MethodHead
-//case "PUT":
-//httpMethod = http.MethodPut
-//case "PATCH":
-//httpMethod = http.MethodPatch
-//case "DELETE":
-//httpMethod = http.MethodDelete
-//case "OPTIONS":
-//httpMethod = http.MethodOptions
-//case "Handle":
-//httpMethod = constant.StringVal(pkg.TypesInfo.Types[callexpr.Args[0]].Value)
-//pathArg = callexpr.Args[1]
-//default:
-//return nil, false
-//}
-
-//// extract the http path + path parameters
-//httpPath, httpPathParams, err := extractPathAndPathParams(pathArg, pkg)
-//if err != nil {
-//return nil, false
-//}
-
-//// description
-//var description string
-//if len(gitRoot) > 0 && len(gitHash) > 0 && len(githubUserRepo) > 0 {
-//description = fmt.Sprintf(
-//"Source: [https://github.com/%s/blob/%s/%s#L%d-L%d]",
-//githubUserRepo,
-//gitHash,
-//path.Clean("."+strings.TrimPrefix(handlerArgEndPos.Filename, gitRoot)),
-//handlerArgStartPos.Line,
-//handlerArgEndPos.Line,
-//)
-//} else {
-//description = fmt.Sprintf("%s:%d", handlerArgStartPos.Filename, handlerArgStartPos.Line)
-//}
-
-//return &Endpoint{
-//Group: Group{
-//ASTNode:    n,
-//Path:       httpPath,
-//PathParams: httpPathParams,
-//},
-//Method:      httpMethod,
-//Description: description,
-//}, true
-//}
-
-//var pathAndPathParamsRegexp = regexp.MustCompilePOSIX(`\/[*:][^\/]+`)
-
-//func extractPathAndPathParams(expr ast.Expr, pkg *packages.Package) (string, openapi3.Parameters, error) {
-//params := openapi3.Parameters{}
-
-//path := constant.StringVal(pkg.TypesInfo.Types[expr].Value)
-//path = pathAndPathParamsRegexp.ReplaceAllStringFunc(path, func(match string) string {
-//required := match[1] == ':'
-//name := match[2:]
-
-//params = append(params, &openapi3.ParameterRef{
-//Value: &openapi3.Parameter{
-//In:       openapi3.ParameterInPath,
-//Name:     name,
-//Required: required,
-//Schema: &openapi3.SchemaRef{
-//Value: &openapi3.Schema{
-//Type: openapi3.TypeString,
-//},
-//},
-//},
-//})
-
-//return "/{" + name + "}"
-//})
-
-//return "/" + strings.TrimLeft(strings.TrimRight(path, "/"), "/"), params, nil
-//}
-
-//func extractEdge(n ast.Node, pkg *packages.Package) (*ast.Ident, ast.Expr, bool) {
-//if assignstmt, ok := n.(*ast.AssignStmt); ok {
-//for i, lhs := range assignstmt.Lhs {
-//if len(assignstmt.Rhs) > i {
-//if ident, ok := lhs.(*ast.Ident); ok {
-//if def := pkg.TypesInfo.Defs[ident]; def != nil {
-//if kind := resolveGinKind(def.Type()); kind != Unsupported {
-//return ident, assignstmt.Rhs[i], true
-//}
-//}
-
-//if use := pkg.TypesInfo.Uses[ident]; use != nil {
-//if kind := resolveGinKind(use.Type()); kind != Unsupported {
-//return ident, assignstmt.Rhs[i], true
-//}
-//}
-//}
-//}
-//}
-//}
-
-//if valuespec, ok := n.(*ast.ValueSpec); ok {
-//for i, ident := range valuespec.Names {
-//if len(valuespec.Values) > i {
-//if def := pkg.TypesInfo.Defs[ident]; def != nil {
-//if kind := resolveGinKind(def.Type()); kind != Unsupported {
-//return ident, valuespec.Values[i], true
-//}
-//}
-
-//if use := pkg.TypesInfo.Uses[ident]; use != nil {
-//if kind := resolveGinKind(use.Type()); kind != Unsupported {
-//return ident, valuespec.Values[i], true
-//}
-//}
-//}
-//}
-//}
-
-//return nil, nil, false
-//}
