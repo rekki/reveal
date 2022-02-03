5c09741888b1c4426049cd418abea12d921c40ad
Aymeric Beaumet <hi@aymericbeaumet.com>
2022-02-03 12:38:30 +0100

    wip

--- tests/gin-nested/main.go
+++ tests/gin-nested/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rekki/reveal/tests/gin-nested/a"
-	"github.com/rekki/reveal/tests/gin-nested/a/b"
 )
 
 func main() {
@@ -12,7 +11,6 @@ func main() {
 	router.GET("endpoint", func(c *gin.Context) {})
 
 	a.Up(router)
-	b.Up(&router.RouterGroup)
 
 	if err := router.Run(":8080"); err != nil {
 		panic(err)
